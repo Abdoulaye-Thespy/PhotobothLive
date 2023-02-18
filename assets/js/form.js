74be0b6175e75aeddfe7912787ea045502cc95d8
Abdoulaye-MAcOS <njigouhrazak@iut-dhaka.edu>
2023-02-18 10:16:34 +0100

    feat: Add new js file

---
+++ assets/js/form.js
@@ -0,0 +1,6 @@
+/**
+* Template Name: PhotoFolio - v1.1.1
+* Template URL: https://bootstrapmade.com/photofolio-bootstrap-photography-website-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
