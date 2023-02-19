6b986c93c276f949e91d66d9476d8300c5e86b37
Abdoulaye-MAcOS <njigouhrazak@iut-dhaka.edu>
2023-02-19 18:01:29 +0100

    feat: Send name to the Endpoint

--- assets/js/form.js
+++ assets/js/form.js
@@ -4,3 +4,31 @@
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
 */
+
+const submitForm = () => {
+  console.log("submit this form")
+  
+// POST request using fetch()
+fetch("https://6kmiwnu3oa.execute-api.us-east-1.amazonaws.com/360GlamEmail", {
+     
+    // Adding method type
+    method: "POST",
+     
+    // Adding body or contents to send
+    body: JSON.stringify({
+        name: "foo",
+    }),
+     
+    // Adding headers to the request
+    headers: {
+        "Content-type": "application/json; charset=UTF-8"
+    }
+})
+ 
+// Converting to JSON
+.then(response => response.json())
+ 
+// Displaying results to console
+.then(json => console.log(json));
+  
+}
